de3508461c55b2f72718473e4b1f104d1f1084f9
Magnus <magk@tuta.io>
2025-12-28 15:13:18 +0000

    fix: stop ghoppers from taking mite food when using transfer API

--- mod/src/main/java/holiday/mixin/InventorySlotWrapperMixin.java
+++ mod/src/main/java/holiday/mixin/InventorySlotWrapperMixin.java
@@ -1,6 +1,7 @@
 package holiday.mixin;
 
 import holiday.block.blockentity.GoldenHopperBlockEntity;
+import holiday.item.HopperMiteItem;
 import net.fabricmc.fabric.impl.transfer.item.InventoryStorageImpl;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
@@ -20,6 +21,7 @@ public abstract class InventorySlotWrapperMixin {
 
     @ModifyArg(method = "extract(Lnet/fabricmc/fabric/api/transfer/v1/item/ItemVariant;JLnet/fabricmc/fabric/api/transfer/v1/transaction/TransactionContext;)J", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/transfer/v1/item/base/SingleStackStorage;extract(Lnet/fabricmc/fabric/api/transfer/v1/item/ItemVariant;JLnet/fabricmc/fabric/api/transfer/v1/transaction/TransactionContext;)J"))
     private long goldenHopperBiggerExtract(long size) {
+        if (HopperMiteItem.isFood(((InventoryStorageImplAccessor)storage).getInventory(), getStack())) return 0;
         if (((InventoryStorageImplAccessor)storage).getInventory() instanceof GoldenHopperBlockEntity) return getStack().getCount();
         return size;
     }
